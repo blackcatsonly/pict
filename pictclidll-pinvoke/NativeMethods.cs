86fb788f1c9a0ce507c2588b23cd3b2696df031f
Forrest Dillaway <forrestd@microsoft.com>
2020-06-23 14:54:16 -0700

    Add a managed Console app that shows how to p/Invoke PICT's 'execute' method in-proc.

---
+++ pictclidll-pinvoke/NativeMethods.cs
@@ -0,0 +1,94 @@
+﻿//---------------------------------------------------------------------------
+// <summary>
+// Wraps p/Invoke declarations for the PICT unmanaged utilities.
+// </summary>
+// <copyright file="NativeMethods.cs" company="Microsoft">
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//---------------------------------------------------------------------------
+
+namespace PICT
+{
+    using System;
+    using System.ComponentModel;
+    using System.Runtime.InteropServices;
+    using System.Text;
+
+    /// <summary>
+    /// Class <see cref="NativeMethods"/> declares the p/Invoke syntax for PICT
+    /// usage from .NET consumers.
+    /// </summary>
+    public static class NativeMethods
+    {
+        /// <summary>
+        /// Executes PICT in-proc with the specified command-line <paramref name="args"/>.
+        /// </summary>
+        /// <param name="args">
+        /// The PICT command-line arguments.
+        /// </param>
+        /// <returns>
+        /// The output produced by PICT.
+        /// </returns>
+        /// <exception cref="Win32Exception">
+        /// PICT did not return ERROR_SUCCESS.
+        /// </exception>
+        public static string Execute(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(paramName: nameof(args));
+            }
+
+            const int ExtraExpectedParameterCount = 1;
+            string[] input = new string[args.Length + ExtraExpectedParameterCount];
+            input[0] = "pict"; // This value does not matter but is expected by PICT.
+            Array.ConstrainedCopy(
+                sourceArray: args,
+                sourceIndex: 0,
+                destinationArray: input,
+                destinationIndex: ExtraExpectedParameterCount,
+                length: args.Length);
+
+            StringBuilder output = new StringBuilder(8192);
+            int ret = Execute(input.Length, input, output, output.Capacity);
+            switch (ret)
+            {
+                case 0:
+                    return output.ToString();
+                default:
+                    throw new Win32Exception(ret);
+            }
+        }
+
+        /// <summary>
+        /// Execute is the entry point to the command-line PICT, and it behaves accordingly.
+        /// To use it, we need to mimic the runtime's handling of the console apps.  The second
+        /// argument is a path to the model file.
+        /// </summary>
+        /// <param name="argCount">
+        /// The number of arguments being passed to PICT
+        /// </param>
+        /// <param name="args">
+        /// The PICT command-line arguments. The first argument is the name of the program but in
+        /// this case, the actual value doesn't matter.
+        /// </param>
+        /// <param name="output">
+        /// Gets set to the output produced by PICT.
+        /// </param>
+        /// <param name="outputLength">
+        /// The length of the <paramref name="output"/> buffer.
+        /// </param>
+        /// <returns>
+        /// The Error Code resulting from executing PICT with the command-line <paramref name="args"/>.
+        /// </returns>
+        /// <remarks>
+        /// pict.dll exports this method, make sure you have pict.dll next to this executable when running.
+        /// </remarks>
+        [DllImport("pict.dll", EntryPoint = "executeNet", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        private static extern int Execute(
+            [In]int argCount,
+            [In][MarshalAs(UnmanagedType.LPArray)] string[] args,
+            [In][Out]StringBuilder output,
+            [In]int outputLength);
+    }
+}
---
+++ pictclidll-pinvoke/Program.cs
@@ -0,0 +1,48 @@
+﻿//---------------------------------------------------------------------------
+// <summary>
+// Sample program  p/Invoke declarations for the PICT unmanaged utilities.
+// </summary>
+// <copyright file="NativeMethods.cs" company="Microsoft">
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//---------------------------------------------------------------------------
+
+namespace PICT
+{
+    using System;
+    using System.ComponentModel;
+
+    /// <summary>
+    /// Class <see cref="Program"/> p/invokes PICT in-proc.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the <see cref="Program"/>,
+        /// invokes <see cref="NativeMethods.Execute(string[])"/> to run PICT
+        /// with the specified <paramref name="args"/>.
+        /// </summary>
+        /// <param name="args">
+        /// The command-line arguments that should be passed to PICT.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Environment.ExitCode"/> produced by PICT.
+        /// </returns>
+        private static int Main(string[] args)
+        {
+            int exitCode;
+            try
+            {
+                string output = NativeMethods.Execute(args);
+                Console.WriteLine(output);
+                exitCode = 0;
+            }
+            catch (Win32Exception e)
+            {
+                exitCode = e.ErrorCode;
+            }
+
+            return exitCode;
+        }
+    }
+}
